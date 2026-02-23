ed335510953468b62293480eb8d0026dabad5063
karoldiaz0627-ops <karoldiaz0627@gmail.com>
2026-02-23 10:39:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,20 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM customers")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
